fdd11aed13e58bfb40ee887f9ca8146fc084f0b5
Suraj <gsnv.suraj@gmail.com>
2021-06-09 18:32:38 +0530

    some contact page changes

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -10,32 +10,40 @@ class Contact extends Component {
 					
                     <div className="formBody">
 
-						<div className="formHeading">
-							<h1>
-								Contact Me
-							</h1>
+						<div className="formLeft">
+							<img src="https://placebeard.it/600x400" alt="placeholder image" />
 						</div>
 
-						<form className="form-inline">
+						<div className="formRight">
 
-							<div className="form-group form-elements">
-								<label className="sr-only" htmlFor="inputName">Name</label>
-								<input type="text" className="form-control" id="inputName" placeholder="Name" required />
+							<div className="formHeading">
+								<h1>
+									Contact Me
+								</h1>
 							</div>
 
-							<div className="form-group form-elements">
-								<label className="sr-only" htmlFor="inputEmail">Email Address</label>
-								<input type="email" className="form-control" id="inputEmail" placeholder="Email" required />
-							</div>
+							<form className="form-inline">
 
-							<div className="form-group form-elements">
-								<label className="sr-only" htmlFor="inputMessage">Message</label>
-								<textarea className="form-control" rows="3" id="inputMessage" placeholder="Leave a Message" required></textarea>
-							</div>
+								<div className="form-group form-elements">
+									<label className="sr-only" htmlFor="inputName">Name</label>
+									<input type="text" className="form-control" id="inputName" placeholder="Name" required />
+								</div>
 
-							<button type="submit" className="btn btn-primary" id="sendBtn">Send</button>
+								<div className="form-group form-elements">
+									<label className="sr-only" htmlFor="inputEmail">Email Address</label>
+									<input type="email" className="form-control" id="inputEmail" placeholder="Email" required />
+								</div>
+
+								<div className="form-group form-elements">
+									<label className="sr-only" htmlFor="inputMessage">Message</label>
+									<textarea className="form-control" rows="4" id="inputMessage" placeholder="Leave a Message" required></textarea>
+								</div>
+
+								<button type="submit" className="btn btn-primary" id="sendBtn">Send</button>
 
 							</form>
+
+						</div>
 
                     </div>
 
