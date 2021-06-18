278f4bac8b9138d4e2907b58b07a16ceb5a47120
Suraj <gsnv.suraj@gmail.com>
2021-06-18 18:14:17 +0530

    added home page

---
+++ src/component/Home.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import '../styles/Home.css';
+
+import Intro from './Intro';
+import Timeline from './Timeline';
+import Contact from './Contact';
+import Projects from './Projects';
+import About from './About';
+
+const Home = () => {
+    return ( 
+        <>
+
+            <div className="homeBody">
+                
+                <div className="introPage">
+                    <Intro />
+                </div>
+
+                <div className="aboutPage">
+                    <About />
+                </div>
+
+                <div className="timelinePage">
+                    <Timeline />
+                </div>
+
+                <div className="projectsPage">
+                    <Projects />
+                </div>
+
+                <div className="contactPage">
+                    <Contact />
+                </div>
+
+            </div>
+        </>
+    );
+}
+ 
+export default Home;
--- src/component/MainContent.jsx
+++ src/component/MainContent.jsx
@@ -6,14 +6,15 @@ import Timeline from './Timeline';
 import Contact from './Contact';
 import Projects from './Projects';
 import About from './About';
+import Home from './Home';
 
 class Content extends Component {
 
     render() {
         return (
             <Switch>
-                <Route path="/" exact component = {Intro} />
-                <Route path="/home" component = {Intro} />
+                <Route path="/" exact component = {Home} />
+                <Route path="/home" component = {Home} />
                 <Route path="/about" component = {About} />
                 <Route path="/timeline" component = {Timeline} />
                 <Route path="/projects" component = {Projects} />
