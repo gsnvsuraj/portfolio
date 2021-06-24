d1eb304e35232928483a9e88b13ecc8680cd5d83
Suraj <gsnv.suraj@gmail.com>
2021-06-24 18:57:24 +0530

    minor changes

--- src/component/About.jsx
+++ src/component/About.jsx
@@ -1,4 +1,6 @@
 import { Component } from 'react';
+import { FaPython, FaJava, FaBootstrap, FaReact, FaGitAlt, FaWindows, FaLinux, FaUbuntu } from 'react-icons/fa';
+import { SiApachekafka, SiPostgresql, SiMicrosoftoffice, SiDocker, SiQuarkus } from 'react-icons/si';
 
 
 class About extends Component {
@@ -8,10 +10,38 @@ class About extends Component {
                 <div className="aboutBody">
                     
                     <div>
-                        <div>
+                        <div className="aboutHeading">
                             <h1>About</h1>
                         </div>
 
+
+
+                    </div>
+
+                    <div className="skilsPage">
+                        <div className="skillsHeading">
+                            <h1>Skills</h1>
+                        </div>
+
+                        <div className="skillsBody">
+                            <div className="categoryTitle">Programming Languages</div>
+                            <FaPython />
+                            <FaJava />
+                            <div className="categoryTitle">Web Technologies</div>
+                            <FaBootstrap />
+                            <FaReact />
+                            <SiPostgresql />
+                            <div className="categoryTitle">Other Technologies</div>
+                            <SiApachekafka />
+                            <FaGitAlt />
+                            <SiQuarkus />
+                            <SiDocker />
+                            <SiMicrosoftoffice />
+                            <div className="categoryTitle">Operating Systems</div>
+                            <FaWindows />
+                            <FaLinux />
+                            <FaUbuntu />
+                        </div>
                     </div>
 
                 </div>
--- src/component/Menu.jsx
+++ src/component/Menu.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { AiFillHome, AiOutlineFundProjectionScreen } from 'react-icons/ai';
+import { MdTimeline } from 'react-icons/md';
+import { IoPerson, IoMail } from 'react-icons/io5';
 
 
 const Menu = ({ open, setOpen }) => {
@@ -8,11 +11,32 @@ const Menu = ({ open, setOpen }) => {
             <div className={open ? "menuHidden showMenu" : "menuHidden hideMenu"}>
                 <div className="menuItems">
                     <div className="headerLinks">
-                        <NavLink to="/" exact={true} className="navLink" onClick={() => setOpen(!open)} >Home</NavLink>
-                        <NavLink to="/about" exact={true} className="navLink" onClick={() => setOpen(!open)} >About</NavLink>
-                        <NavLink to="/timeline" exact={true} className="navLink" onClick={() => setOpen(!open)} >Timeline</NavLink>
-                        <NavLink to="/projects" exact={true} className="navLink" onClick={() => setOpen(!open)} >Projects</NavLink>
-                        <NavLink to="/contact" exact={true} className="navLink" onClick={() => setOpen(!open)} >Contact</NavLink>
+                        
+                        <NavLink to="/" exact={true} className="navLink" onClick={() => setOpen(false)} >
+                            <span className="navLinkIcons"><AiFillHome /></span>
+                            Home
+                        </NavLink>
+                        
+                        <NavLink to="/about" exact={true} className="navLink" onClick={() => setOpen(false)} >
+                            <span className="navLinkIcons"><IoPerson /></span>
+                            About
+                        </NavLink>
+                        
+                        <NavLink to="/timeline" exact={true} className="navLink" onClick={() => setOpen(false)} >
+                            <span className="navLinkIcons"><MdTimeline /></span>
+                            Timeline
+                        </NavLink>
+                        
+                        <NavLink to="/projects" exact={true} className="navLink" onClick={() => setOpen(false)} >
+                            <span className="navLinkIcons"><AiOutlineFundProjectionScreen /></span>
+                            Projects
+                        </NavLink>
+                        
+                        <NavLink to="/contact" exact={true} className="navLink" onClick={() => setOpen(false)} >
+                            <span className="navLinkIcons"><IoMail /></span>
+                            Contact
+                        </NavLink>
+
                     </div>
                 </div>
             </div>
