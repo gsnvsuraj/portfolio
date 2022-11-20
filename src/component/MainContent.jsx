0b7d9ddfad50aa446bfbc7aa87a50f938903de90
Suraj <gsnv.suraj@gmail.com>
2022-11-20 22:08:09 +0530

    Update React depencencies

--- src/component/MainContent.jsx
+++ src/component/MainContent.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import Projects from './Projects';
 import About from './About';
@@ -10,13 +10,13 @@ import Intro from './Intro';
 const Content = () => {
 
     return (
-        <Switch>
-            <Route path="/" exact component = {Home} />
-            <Route path="/intro" exact component = {Intro} />
-            <Route path="/home" component = {Home} />
-            <Route path="/about" component = {About} />
-            <Route path="/projects" component = {Projects} />
-        </Switch>
+        <Routes>
+            <Route path="/" exact element = {<Home/>} />
+            <Route path="/intro" exact element = {<Intro/>} />
+            <Route path="/home" element = {<Home/>} />
+            <Route path="/about" element = {<About/>} />
+            <Route path="/projects" element = {<Projects/>} />
+        </Routes>
     );
 
 }
